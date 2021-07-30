1e9192e25cd1807584088661b23cb223c9df9548
GerardoZuno <gerardozunounitec@gmail.com>
2021-07-30 16:15:41 -0500

    completed delete task

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import { firebase } from './firebase';
+import React, { useEffect, useState } from "react";
+import { firebase } from "./firebase";
 
 function App() {
-  const [tareas, setTareas] = useState([])
-  const [tarea, setTarea] = useState('')
-
-
+  const [tareas, setTareas] = useState([]);
+  const [tarea, setTarea] = useState("");
 
   useEffect(() => {
     const obtenerDatos = async () => {
       try {
-        const db = firebase.firestore()
-        const data = await db.collection('tareas').get()
-        /*console.log(data.docs)*/        
-        const arrayData = data.docs.map(item => ({id: item.id, ...item.data()}))
-        console.log(arrayData)
-        setTareas(arrayData)
+        const db = firebase.firestore();
+        const data = await db.collection("tareas").get();
+        /*console.log(data.docs)*/
+        const arrayData = data.docs.map((item) => ({
+          id: item.id,
+          ...item.data(),
+        }));
+        console.log(arrayData);
+        setTareas(arrayData);
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     obtenerDatos();
-  }, [])
+  }, []);
 
   const agregar = async (e) => {
-     e.preventDefault()
-     if(!tarea.trim()){
-       console.log('elemento vacio')
-       return
-     }
-     console.log(tarea)
+    e.preventDefault();
+    if (!tarea.trim()) {
+      console.log("elemento vacio");
+      return;
+    }
+    try {
+      const db = firebase.firestore();
+      const nuevaTarea = {
+        name: tarea,
+        fecha: Date.now(),
+      };
+      //add te da un id aleatorio
+      const data = await db.collection("tareas").add(nuevaTarea);
+      console.log(data.docs);
+      setTareas([
+        ...tareas,
+        {...nuevaTarea, id: data.id}
+      ])
+      setTarea("");
+    } catch (error) {
+      console.log(error);
+    }
+    
+  };
+
+  const eliminar = async (id) => {
+      try{
+        const db = firebase.firestore()
+        await db.collection('tareas').doc(id).delete()
+        const arrayFiltrado = tareas.filter((item) => item.id !== id)
+        setTareas(arrayFiltrado)
+
+      }catch(error){
+        console.log(error)
+      }
   }
 
   return (
@@ -38,28 +68,37 @@ function App() {
         <div className="col-md-6">
           Lista las tareas
           <ul className="list-group">
-
-            {
-              tareas.map(item =>(
-                <li className='list-group-item' key={item.id}>
-                  {item.name}
-                </li>
-              ))
-            }
-
+            {tareas.map((item) => (
+              <li className="list-group-item mr-20" key={item.id}>
+                {item.name}
+                <button 
+                className="btn btn-danger btn-sm  float-right"
+                onClick={() =>eliminar(item.id)}
+                >
+                  Eliminar
+                </button>
+                <button className="btn btn-warning btn-sm  float-right mr-2">
+                  Editar
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="col-md-6">
           <h3>Formulario</h3>
           <form onSubmit={agregar}>
-             <input type="text"
-             placeholder="Ingrese Tarea"
-             className="form-control mb-2"
-             onChange={e => setTarea(e.target.value)}
-             value={tarea}
-             />
-             <button type="submit" className="btn btn-block btn-dark">Agregar</button>
+            <input
+              type="text"
+              placeholder="Ingrese Tarea"
+              className="form-control mb-2"
+              onChange={(e) => setTarea(e.target.value)}
+              value={tarea}
+            />
+            <button type="submit" className="btn btn-block btn-dark">
+              Agregar
+            </button>
           </form>
+          
         </div>
       </div>
     </div>
