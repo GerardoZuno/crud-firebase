4365641bde29d8e36c001d3f3c193bc980159683
GerardoZuno <gerardozunounitec@gmail.com>
2021-07-30 17:15:27 -0500

    Completed Crud

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { firebase } from "./firebase";
 function App() {
   const [tareas, setTareas] = useState([]);
   const [tarea, setTarea] = useState("");
+  const [modoEdicion, setModoEdicion] = useState(false);
+  const [id, setId] = useState("");
 
   useEffect(() => {
     const obtenerDatos = async () => {
@@ -39,28 +41,64 @@ function App() {
       //add te da un id aleatorio
       const data = await db.collection("tareas").add(nuevaTarea);
       console.log(data.docs);
-      setTareas([
-        ...tareas,
-        {...nuevaTarea, id: data.id}
-      ])
+      setTareas([...tareas, { ...nuevaTarea, id: data.id }]);
       setTarea("");
     } catch (error) {
       console.log(error);
     }
-    
+  };
+
+  const activarEdicion = (item) => {
+    setModoEdicion(true);
+    setTarea(item.name);
+    setId(item.id);
+
+    console.log(item);
+  };
+
+  const editar = async (e) => {
+    e.preventDefault();
+    if (!tarea.trim()) {
+      console.log("elemento vacio");
+      return;
+    }
+    try {
+
+      const db = firebase.firestore();
+      await db.collection("tareas").doc(id).update({
+        name: tarea        
+      })
+      const arrayEditado = tareas.map((item) =>(
+          item.id === id ? {
+            id: item.id,
+            fecha: item.fecha,
+            name: tarea
+          } : item
+      ))
+      setTareas(arrayEditado)
+      setModoEdicion(false);
+      setTarea('');
+      setId('');
+
+
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const eliminar = async (id) => {
-      try{
-        const db = firebase.firestore()
-        await db.collection('tareas').doc(id).delete()
-        const arrayFiltrado = tareas.filter((item) => item.id !== id)
-        setTareas(arrayFiltrado)
+    setModoEdicion(false);
+    setTarea("");
 
-      }catch(error){
-        console.log(error)
-      }
-  }
+    try {
+      const db = firebase.firestore();
+      await db.collection("tareas").doc(id).delete();
+      const arrayFiltrado = tareas.filter((item) => item.id !== id);
+      setTareas(arrayFiltrado);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="container mt-3">
@@ -71,13 +109,16 @@ function App() {
             {tareas.map((item) => (
               <li className="list-group-item mr-20" key={item.id}>
                 {item.name}
-                <button 
-                className="btn btn-danger btn-sm  float-right"
-                onClick={() =>eliminar(item.id)}
+                <button
+                  className="btn btn-danger btn-sm  float-right"
+                  onClick={() => eliminar(item.id)}
                 >
                   Eliminar
                 </button>
-                <button className="btn btn-warning btn-sm  float-right mr-2">
+                <button
+                  className="btn btn-warning btn-sm  float-right mr-2"
+                  onClick={() => activarEdicion(item)}
+                >
                   Editar
                 </button>
               </li>
@@ -85,8 +126,9 @@ function App() {
           </ul>
         </div>
         <div className="col-md-6">
-          <h3>Formulario</h3>
-          <form onSubmit={agregar}>
+          <h3>{modoEdicion ? "Editar" : "Formulario"}</h3>
+
+          <form onSubmit={modoEdicion ? editar : agregar}>
             <input
               type="text"
               placeholder="Ingrese Tarea"
@@ -94,11 +136,17 @@ function App() {
               onChange={(e) => setTarea(e.target.value)}
               value={tarea}
             />
-            <button type="submit" className="btn btn-block btn-dark">
-              Agregar
+            <button
+              type="submit"
+              className={
+                modoEdicion
+                  ? "btn btn-block btn-warning"
+                  : "btn btn-block btn-dark"
+              }
+            >
+              {modoEdicion ? "Editar" : "Agregar"}
             </button>
           </form>
-          
         </div>
       </div>
     </div>
